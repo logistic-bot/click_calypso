80322a23ec95b2fe55f9b9da399cad346878bb32
Khais COLIN <kh.col@orange.fr>
2020-04-07 21:03:08 +0200

    Calypso v2.3.0 Scatter
    basic editing capabilities

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ def version():
     """Prints version and copyright information"""
     click.echo("You are using CALYPSO, originally coded by Khaïs COLIN.")
     click.echo("Copyright: GNU GPL 3.0")
-    click.echo("Calypso v2.2.0 `deep`")
+    click.echo("Calypso v2.3.0 `Scatter`")
 
 
 if __name__ == "__main__":
